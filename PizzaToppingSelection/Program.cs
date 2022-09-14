766a5a4afe03f0fe95964641a3f9350e4ed68f87
William Harris <17HarriW@fulford.york.sch.uk>
2022-09-14 12:26:16 +0100

    Added functions

--- PizzaToppingSelection/Program.cs
+++ PizzaToppingSelection/Program.cs
@@ -10,8 +10,7 @@ namespace PizzaToppingSelection
     {
         static void Main(string[] args)
         {
-            Console.Write("What pizza topping would you like?: ");
-            string topping = Console.ReadLine().ToLower();
+            string topping = Input("What pizza topping would you like?: ").ToLower();
 
             switch(topping)
             {
@@ -28,7 +27,40 @@ namespace PizzaToppingSelection
                     break;
             }
 
+            string music = Input("What type of music do you like?: ");
+
+            switch(music)
+            {
+                case "rock":
+                case "jazz":
+                    Console.WriteLine("Respectable if you get something good");
+                    break;
+                case "rap":
+                case "drill":
+                    Console.WriteLine("I hate you");
+                    break;
+                case "pop":
+                case "edm":
+                case "trance":
+                    Console.WriteLine("Hmm, call me");
+                    break;
+                default:
+                    Console.WriteLine("Wat?");
+                    break;
+            }
+
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Ask user for an input
+        /// </summary>
+        /// <param name="question"> The input being asked </param>
+        /// <returns> The answer to the input </returns>
+        private static string Input(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
         }
     }
 }
